2b5d98457a3a65c7fa5468199f8759425b27d6bc
Some_Meme <bjorge.andrea@gmail.com>
2020-09-08 13:13:34 +0200

    Rettet opp noen småfeil i oppgave 8

--- no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -91,34 +91,24 @@ public class Oblig1 {
     ///// Oppgave 8 //////////////////////////////////////
     public static int[] indekssortering(int[] a) {
         int [] indekser = new int [a.length];
-        int [] hjelpetabell = new int[a.length];
-
-        System.arraycopy(a, 0, hjelpetabell, 0, a.length);
-
-        int minsteverdi;
-        int minsteverdiindeks;
 
         for(int i = 0; i<a.length; ++i){
-            minsteverdi = hjelpetabell[i];
-            minsteverdiindeks = i;
-            for(int j = i+1; j < a.length; ++j){
-                if(hjelpetabell[j]<minsteverdi) {
-                    minsteverdi = hjelpetabell[j];
-                    minsteverdiindeks = j;
-                }
-            }
-            int midlertidig = hjelpetabell[i];
-            hjelpetabell[i] = minsteverdi;
-            hjelpetabell[minsteverdiindeks] = midlertidig;
+            indekser[i]=i;
         }
 
-        for(int i = 0; i <a.length; ++i){
-            for(int j = 0; j<a.length; ++j){
-                if(hjelpetabell[i]==a[j] && (i<1 || indekser[i-1] != j)){
-                    indekser[i] = j;
+
+        for(int  i = 0; i<a.length; ++i){
+            for(int j = i+1; j<a.length; ++j){
+                int midlertidig;
+
+                if(a[indekser[i]]>a[indekser[j]]){
+                    midlertidig = indekser[i];
+                    indekser[i] = indekser[j];
+                    indekser[j] = midlertidig;
                 }
             }
         }
+
         return indekser;
     }
 
