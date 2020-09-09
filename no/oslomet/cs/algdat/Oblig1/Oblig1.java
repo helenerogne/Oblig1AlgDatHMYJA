2dd0fc9672c5b6cda58d12bc2afa25e3ff312c6a
yaldashams <56063649+yaldashams@users.noreply.github.com>
2020-09-09 14:10:19 +0200

    NY oppg 10

--- no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -6,11 +6,12 @@ import java.lang.UnsupportedOperationException;
 import java.util.NoSuchElementException;
 
 public class Oblig1 {
-    private Oblig1() {}
+    private Oblig1() {
+    }
 
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
-        if (a == null ) {
+        if (a == null) {
             throw new NoSuchElementException("Det finnes ingen liste");
         }
 
@@ -25,7 +26,7 @@ public class Oblig1 {
         }
 
 
-        return  a[a.length-1];
+        return a[a.length - 1];
     }
 
     public static int ombyttinger(int[] a) {
@@ -61,55 +62,54 @@ public class Oblig1 {
 
     ///// Oppgave 4 //////////////////////////////////////
     public static void delsortering(int[] a) {
-            int indeks = parterOddPar(a, 0, a.length-1);
-            quickSort(a, 0, indeks-1);
-            quickSort(a, indeks, a.length-1);
-            //delsortering(a, begin, indeks - 1);
-            //delsortering(a, indeks + 1, end);
-        }
+        int indeks = parterOddPar(a, 0, a.length - 1);
+        quickSort(a, 0, indeks - 1);
+        quickSort(a, indeks, a.length - 1);
+        //delsortering(a, begin, indeks - 1);
+        //delsortering(a, indeks + 1, end);
+    }
 
-        private static int partition(int[] a, int begin, int end){
-            int pivot = a[end];
-            int j = (begin-1);
+    private static int partition(int[] a, int begin, int end) {
+        int pivot = a[end];
+        int j = (begin - 1);
 
-            for(int i = begin; i < end; i++){
-                if(a[i] <= pivot){
-                    j++;
-                    bytt(a, i, j);
-                }
-            }
-            bytt(a, j+1, end);
-            return j+1;
-        }
-
-        private static void quickSort(int[] values, int left, int right){
-            if(left < right){
-                int pivot_index = partition(values, left, right);
-                quickSort(values, left, pivot_index-1);
-                quickSort(values, pivot_index+1, right);
+        for (int i = begin; i < end; i++) {
+            if (a[i] <= pivot) {
+                j++;
+                bytt(a, i, j);
             }
         }
+        bytt(a, j + 1, end);
+        return j + 1;
+    }
+
+    private static void quickSort(int[] values, int left, int right) {
+        if (left < right) {
+            int pivot_index = partition(values, left, right);
+            quickSort(values, left, pivot_index - 1);
+            quickSort(values, pivot_index + 1, right);
+        }
+    }
 
 
-        public static int parterOddPar(int [] a, int begin, int end){
-            while (true) {
-                while (begin<=end  &&  a[begin] % 2 != 0) begin++;
-                while (begin<=end  && a[end] % 2 == 0) end--;
-                if (begin < end) {
-                    bytt(a, begin++, end--);
+    public static int parterOddPar(int[] a, int begin, int end) {
+        while (true) {
+            while (begin <= end && a[begin] % 2 != 0) begin++;
+            while (begin <= end && a[end] % 2 == 0) end--;
+            if (begin < end) {
+                bytt(a, begin++, end--);
 
-                }
-                else {
-                    return begin;
-                }
+            } else {
+                return begin;
             }
         }
+    }
 
-        public static void bytt(int [] a, int i, int j) {
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-        }
+    public static void bytt(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
 
     ///// Oppgave 5 //////////////////////////////////////
@@ -145,38 +145,32 @@ public class Oblig1 {
 
     ///// Oppgave 10 ////////////////////////////////////// throw new UnsupportedOperationException();
 
-   public static int bokstavNr(char bokstav) {
+   /*public static int bokstavNr(char bokstav) {
         throw new UnsupportedOperationException();
     }
 
-    public static boolean inneholdt(String a, String b) {
-          String characters = "";
-          int count = 0;
+    */
+    ///// Oppgave 10 ////////////////////////////////////// throw new UnsupportedOperationException();
 
+    /*public static int bokstavNr(char bokstav) {
+        throw new UnsupportedOperationException();
+    }
+     */
+
+    public static boolean inneholdt(String a, String b) {
         if (a.length() == 0 && b.length() == 0) {
             return true;
         }
-        if ((a.length() != 0 && b.length() == 0) || a.length()>b.length()) {
+        if ((a.length() != 0 && b.length() == 0) || a.length() > b.length()) {
             return false;
         }
-        /*
-          b = Oblig1.inneholdt("ABBA", "BARBERER");
-        if (b != false) {
-            System.out.println
-                    ("Oppgave 10: j) Svaret skal bli lik false her!");
-            antallFeil++;
-        }
-         */
 
         for (int i = 1; i < b.length(); i++) {
             for (int j = 0; j < a.length(); j++) {
-                String word = Character.toString(b.charAt(i-1));
+                String word = Character.toString(b.charAt(i - 1));
                 String dublicate = Character.toString(a.charAt(j));
 
                 if (!dublicate.contains(word)) {
-                    count++;
-                    i++;
-                    characters += dublicate;
                     return true;
                 }
 
@@ -186,17 +180,11 @@ public class Oblig1 {
 
             }
         }
-        //System.out.println(characters);
-        //System.out.println(count);
         return true;
     }
-    //
-    /*public static void main (String[] args) {
-        inneholdt("xyyx", "BARxByEyRER");
-    }
-
-     */
 }
-
-
 // Oblig1
+
+
+
+
